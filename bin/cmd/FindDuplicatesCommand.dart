7400ee05499f9890c0d96c04e35a7963c4a24136
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-01-31 20:10:37 +0300

    added fileFormats and additional file check

--- bin/cmd/FindDuplicatesCommand.dart
+++ bin/cmd/FindDuplicatesCommand.dart
@@ -1,11 +1,23 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:path/path.dart' show extension;
 import 'package:args/command_runner.dart';
 import 'package:sff_lib/sff_lib.dart';
 
 class FindDuplicatesCommand extends Command<String> {
+  final  _fileFormats = {
+    'all':    (_) => true,
+    'img':    (path) => ['.jpeg', '.png', '.webp', '.gif', '.raw', '.tiff', '.psd'].contains(extension(path)),
+    'vector': (path) => ['.svg', '.esp', '.pdf', '.ai', '.cdr'].contains(extension(path)),
+    'video':  (path) => ['.avi', '.mkv', '.mp4', '.mpeg', '.ogv', '.webm'].contains(extension(path)),
+    'audio':  (path) => ['.m4a', '.mp3', '.wav', '.ogg', '.mpa', '.flac'].contains(extension(path)),
+    'doc':  (path) => ['.pdf', '.djvy', '.doc', '.docx', '.txt', '.md'].contains(extension(path)),
+  };
+
   FindDuplicatesCommand() {
+    argParser.addFlag('cfe', help: 'additional check of the file contents');
+    argParser.addOption('format', allowed: [..._fileFormats.keys], defaultsTo: "all");
     argParser.addOption('dir', help: 'Directory where to start the find');
   }
 
@@ -23,8 +35,17 @@ class FindDuplicatesCommand extends Command<String> {
     if (argResults?["dir"] case String path) {
       final dir = Directory(path);
       if (dir.existsSync()) {
-        findDuplicates(dir).listen((event) { 
-          print("----------\n${event.$1.path}\n${event.$1.path}");
+        findDuplicates(dir, _fileFormats[argResults?["format"]]).listen((event) { 
+          if (argResults!.wasParsed("cfe")) {
+            compareFilesEquality(event.$1, event.$2).then((isEq) {
+              if (isEq){
+                print("----------\n${event.$1.path}\n${event.$1.path}");
+              }
+            });
+          }
+          else {
+            print("----------\n${event.$1.path}\n${event.$1.path}");
+          }
         });
         return "ok";
       } else {
