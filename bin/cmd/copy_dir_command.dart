6b173942f7d264c544bc66b07ff026b7b6a5fc63
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-02-16 22:28:04 +0400

    added new command copy dir rec and rn files

---
+++ bin/cmd/copy_dir_command.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:args/command_runner.dart';
+import 'package:sff_lib/sff_lib.dart';
+
+import '../helper/file_print.dart';
+
+class CopyDirCommand extends Command<String> {
+  CopyDirCommand() {
+    argParser.addOption('dirin', help: 'Path to directory in');
+    argParser.addOption('dirout', help: 'Path to directory out');
+  }
+
+  @override
+  String get name => 'CopyDirRec';
+
+  @override
+  String get description => 'Copy Directory Recursive';
+
+  @override
+  List<String> get aliases => ['cdr'];
+
+  @override
+  FutureOr<String>? run() {
+    if (argResults?["dirin"] case String pathIn) {
+      if (argResults?["dirout"] case String pathOut) {
+        final dirIn = Directory(pathIn);
+        final dirOut = Directory(pathOut);
+
+        if (dirIn.existsSync()) {
+          copyDirRec(dirIn, dirOut).listen((event) {
+            printDuoFilePath(event);
+          });
+          return "ok";
+        } else {
+          throw "Dir1 does not exist";
+        }
+      }
+    }
+    return null;
+  }
+}
--- bin/sff_cli.dart
+++ bin/sff_cli.dart
@@ -1,20 +1,23 @@
 import 'package:args/command_runner.dart';
 
-import 'cmd/CompareFilesEqualityCommand.dart';
-import 'cmd/FindDuplicatesCommand.dart';
-import 'cmd/SyncDirCommand.dart';
+import 'cmd/compare_files_equality_command.dart';
+import 'cmd/find_duplicates_command.dart';
+import 'cmd/sync_dir_command.dart';
+import 'cmd/copy_dir_command.dart';
 
 void main(List<String> args) async {
-  final runner = CommandRunner<String>('sff', 'Easy work with files/directories')
-    ..addCommand(FindDuplicatesCommand())
-    ..addCommand(SyncDirCommand())
-    ..addCommand(CompareFilesEqualityCommand());
+  final runner =
+      CommandRunner<String>('sff', 'Easy work with files/directories')
+        ..addCommand(FindDuplicatesCommand())
+        ..addCommand(SyncDirCommand())
+        ..addCommand(CompareFilesEqualityCommand())
+        ..addCommand(CopyDirCommand());
 
   try {
     if (await runner.run(args) == null) {
       print(runner.usage);
     }
-  } catch(e){
+  } catch (e) {
     print(e);
   }
 }
