703e56b413b264bbd0ee7997f9cc2ad7dea11039
Miomit <movsesyan-03@inbox.ru>
2024-05-25 14:55:05 +0000

    added file formats

--- bin/cmd/copy_dir_command.dart
+++ bin/cmd/copy_dir_command.dart
@@ -2,15 +2,41 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:args/command_runner.dart';
+import 'package:path/path.dart';
 import 'package:sff_lib/sff_lib.dart';
 
 import '../helper/file_print.dart';
 
 class CopyDirCommand extends Command<String> {
+
+  final _fileFormats = {
+    'all': (_) => true,
+    'img': (path) => [
+          '.jpeg',
+          '.jpg',
+          '.png',
+          '.webp',
+          '.gif',
+          '.raw',
+          '.tiff',
+          '.psd'
+        ].contains(extension(path)),
+    'vector': (path) =>
+        ['.svg', '.esp', '.pdf', '.ai', '.cdr'].contains(extension(path)),
+    'video': (path) => ['.avi', '.mkv', '.mp4', '.mpeg', '.ogv', '.webm']
+        .contains(extension(path)),
+    'audio': (path) => ['.m4a', '.mp3', '.wav', '.ogg', '.mpa', '.flac']
+        .contains(extension(path)),
+    'doc': (path) => ['.pdf', '.djvy', '.doc', '.docx', '.txt', '.md']
+        .contains(extension(path)),
+  };
+  
   CopyDirCommand() {
     argParser.addOption('dirin', help: 'Path to directory in');
     argParser.addOption('dirout', help: 'Path to directory out');
     argParser.addFlag('cod', help: 'Copy only directories');
+    argParser.addOption('format',
+        allowed: [..._fileFormats.keys], defaultsTo: "all");
   }
 
   @override
@@ -30,7 +56,12 @@ class CopyDirCommand extends Command<String> {
         final dirOut = Directory(pathOut);
 
         if (dirIn.existsSync()) {
-          copyDirRec(dirIn, dirOut, isCopyFile: !argResults!.wasParsed("cod")).listen((event) {
+          copyDirRec(
+            dirIn,
+            dirOut, 
+            isCopyFile: !argResults!.wasParsed("cod"),
+            filter: _fileFormats[argResults?["format"]]
+          ).listen((event) {
             printDuoFilePath(event);
           });
           return "ok";
