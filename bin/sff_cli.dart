e7001aac72d89f71c166930061ec49ef3a8ef98e
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-07-09 16:50:31 +0300

    update path cmd

--- bin/sff_cli.dart
+++ bin/sff_cli.dart
@@ -1,10 +1,10 @@
 import 'package:args/command_runner.dart';
 
-import 'cmd/checksum_command.dart';
-import 'cmd/compare_files_equality_command.dart';
-import 'cmd/find_duplicates_command.dart';
-import 'cmd/sync_dir_command.dart';
-import 'cmd/copy_dir_command.dart';
+import '../cmd/checksum_command.dart';
+import '../cmd/compare_files_equality_command.dart';
+import '../cmd/find_duplicates_command.dart';
+import '../cmd/sync_dir_command.dart';
+import '../cmd/copy_dir_command.dart';
 
 void main(List<String> args) async {
   final runner =
