b91bf4b5451032664f29c78d3dbc7641c1988ca3
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-07-09 20:42:50 +0300

    added RunCommand

--- bin/sff_cli.dart
+++ bin/sff_cli.dart
@@ -5,10 +5,12 @@ import '../cmd/compare_files_equality_command.dart';
 import '../cmd/find_duplicates_command.dart';
 import '../cmd/sync_dir_command.dart';
 import '../cmd/copy_dir_command.dart';
+import '../cmd/run_command.dart';
 
 void main(List<String> args) async {
   final runner =
       CommandRunner<String>('sff', 'Easy work with files/directories')
+        ..addCommand(RunCommand())
         ..addCommand(FindDuplicatesCommand())
         ..addCommand(SyncDirCommand())
         ..addCommand(CompareFilesEqualityCommand())
