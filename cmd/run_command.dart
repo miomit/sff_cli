1cad2ce0a1e2c52b388859504c4e124af707ae05
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-07-09 20:57:53 +0300

    init RunCommand

---
+++ cmd/run_command.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:args/command_runner.dart';
+import 'package:sff_lib/sff_lib.dart' show Sff;
+
+class RunCommand extends Command<String> {
+  @override
+  String get name => 'Run';
+
+  @override
+  String get description => 'Run sff config';
+
+  @override
+  List<String> get aliases => ['run'];
+
+  @override
+  FutureOr<String>? run() async {
+    if (argResults?.arguments.length == 1) {
+      Sff.runConfigFile(
+        argResults!.arguments.first,
+      ).listen(
+        (fl) {
+          print(fl);
+        },
+      );
+      return "ok";
+    } else {
+      throw "specify the path";
+    }
+  }
+}
