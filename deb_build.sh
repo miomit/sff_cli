5c3717e60a438398b45729afcddf9f953479bea8
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-07-14 18:25:23 +0300

    rm deb_build.sh

--- deb_build.sh
+++
@@ -1,9 +0,0 @@
-mkdir build
-mkdir -p ./deb/usr/bin 
-
-rm ./build/sff.deb
-rm ./deb/usr/bin/sff 
-
-dart compile exe ./bin/sff_cli.dart -o ./deb/usr/bin/sff
-
-dpkg-deb --build ./deb/ ./build/sff.deb
